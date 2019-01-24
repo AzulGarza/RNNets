266493e2fc0a90b141ec79dcd9c40d940021602a
Navarro Macías Samuel Fernando <F15134@banxico.org.mx>
2019-01-24 16:43:34 -0600

    rnn forward for m samples

--- rnn_numpy/rnn_scratch.py
+++ rnn_numpy/rnn_scratch.py
@@ -35,3 +35,41 @@ def rnn_cell_forward(xt, h_prev, parameters):
     cache = (h_next, h_prev, xt, parameters)
 
     return h_next, yt_hat, cache
+
+
+def rnn_forward(x, h0, parameters):
+    """
+    Implements the fwd prop of the rnn
+
+    Parameters:
+    -----------
+    x : Input data for every time-step 't', shape (n_x, m, T_x).
+    h0 : Initial hidden state, shape (n_h, m)
+    parameters : python dictionary containing:
+        U : Weight matrix multiplying the input, shape (n_a, n_x)
+        W : Weight matrix multiplying the hidden state, shape (n_a, n_a)
+        V : Weight matrix relating the hidden-state to the output, shape (n_y, n_a)
+        b : Bias, shape (n_a, 1)
+        c : Bias relating the hidden-state to the output, shape (n_y, 1)
+    Returns:
+    h : Hidden states for every time-step, shape (n_a, m, T_x)
+    y_hat : Predictions for every time-step, shape (n_y, m, T_x)
+    caches : tuple needed for backward pass, contains (list of caches, x)
+    """
+    caches = []
+    n_x, m, T_x = x.shape
+    n_y, n_h = parameters["V"].shape
+
+    h = np.zeros((n_h, m, T_x))
+    y_hat = np.zeros((n_y, m, T_x))
+    h_next = h0
+
+    for t in range(T_x):
+        h_next, yt_hat, cache = rnn_cell_forward(x[:,:,t], h_next, parameters)
+        h[:,:,t] = h_next
+        y_hat[:,:,t] = yt_hat
+        caches.append(cache)
+
+    caches = (caches, x)
+
+    return h, y_hat, caches
--- rnn_numpy/test_rnn_scratch.py
+++ rnn_numpy/test_rnn_scratch.py
@@ -1,6 +1,6 @@
 import numpy as np
 import unittest
-from .rnn_scratch import rnn_cell_forward
+from .rnn_scratch import rnn_cell_forward, rnn_forward
 np.random.seed(1)
 
 
@@ -21,10 +21,26 @@ class TestRNN_Fwd(unittest.TestCase):
         h_next, yt_hat, cache = rnn_cell_forward(xt, h_prev, parameters)
 
         self.assertEqual(yt_hat.shape, (2, 10))
-        # self.assertEqual(h_next[4] == [0.59584544 0.18141802 0.61311866 0.99808218 0.85016201 0.99980978 -0.18887155 0.99815551 0.6531151 0.82872037])
+        # self.assertEqual(h_next[4], [0.59584544 0.18141802 0.61311866 0.99808218 0.85016201 0.99980978 -0.18887155 0.99815551 0.6531151 0.82872037])
         self.assertEqual(h_next.shape, (5, 10))
         # print("yt_hat[1] =", yt_hat[1])
-
+    
+    def test_rnn_rwd(self):
+        """
+        Test fwd prop of the rnn
+        """
+        x = np.random.randn(3,10,4)
+        h0 = np.random.randn(5,10)
+        W = np.random.randn(5,5)
+        U = np.random.randn(5,3)
+        V = np.random.randn(2,5)
+        b = np.random.randn(5,1)
+        c = np.random.randn(2,1)
+        parameters = {"U": U, "W": W, "V": V, "b": b, "c": c}
+        h, y_hat, caches = rnn_forward(x, h0, parameters)
+        self.assertEqual(h.shape, (5, 10, 4))
+        self.assertEqual(y_hat.shape, (2, 10, 4))
+        self.assertEqual(len(caches), 2)
 
 if __name__ == '__main__':
     unittest.main()
